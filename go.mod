49853c45f771bd55428ab2b2733ba83559a74178
yohanleb <yohan.lebricquer@gmail.com>
2024-08-19 16:48:23 +0100

    Init project

---
+++ go.mod
@@ -0,0 +1,10 @@
+module api
+
+go 1.23
+
+require github.com/go-chi/chi/v5 v5.1.0
+
+require (
+	go.uber.org/multierr v1.11.0 // indirect
+	go.uber.org/zap v1.27.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"go.uber.org/zap"
+	"net/http"
+	"time"
+)
+
+func main() {
+	zap.ReplaceGlobals(zap.Must(zap.NewProduction()))
+	r := chi.NewRouter()
+
+	// A good base middleware stack
+	r.Use(middleware.Timeout(60 * time.Second))
+	r.Use(middleware.RequestID)
+	r.Use(middleware.RealIP)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+
+	r.Get("/", homePage)
+
+	err := http.ListenAndServe(":1323", r)
+	if err != nil {
+		zap.L().Error("Failed to start the app")
+	}
+	zap.L().Error("App started")
+}
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	_, err := fmt.Fprintf(w, "Welcome !")
+	if err != nil {
+		zap.L().Error("Failed to print Welcome")
+	}
+}
