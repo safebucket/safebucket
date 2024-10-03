11322f5f9aabb998243be3e34b9e367985a80387
yohanleb <yohan.lebricquer@gmail.com>
2024-10-03 15:28:30 +0100

    Improve providers logging

--- internal/configuration/providers.go
+++ internal/configuration/providers.go
@@ -7,7 +7,6 @@ import (
 	"github.com/coreos/go-oidc/v3/oidc"
 	"go.uber.org/zap"
 	"golang.org/x/oauth2"
-	"log"
 )
 
 type Provider struct {
@@ -23,16 +22,14 @@ type ProvidersConfiguration map[string]models.ProviderConfiguration
 func LoadProviders(ctx context.Context, providersCfg ProvidersConfiguration) Providers {
 	var providers = Providers{}
 	for name, providerCfg := range providersCfg {
-		zap.L().Info(
-			"auth provider",
-			zap.String("name", name),
-			zap.String("client_id", providerCfg.ClientId),
-			zap.String("issuer", providerCfg.Issuer),
-		)
-
 		provider, err := oidc.NewProvider(ctx, providerCfg.Issuer)
 		if err != nil {
-			log.Fatal(err)
+			zap.L().Error(
+				"Failed to load provider",
+				zap.String("name", name),
+				zap.Error(err),
+			)
+			continue
 		}
 
 		verifier := provider.Verifier(&oidc.Config{ClientID: providerCfg.ClientId})
@@ -50,6 +47,13 @@ func LoadProviders(ctx context.Context, providersCfg ProvidersConfiguration) Pro
 			Verifier:    verifier,
 			OauthConfig: oauthConfig,
 		}
+
+		zap.L().Info(
+			"Loaded auth provider",
+			zap.String("name", name),
+			zap.String("client_id", providerCfg.ClientId),
+			zap.String("issuer", providerCfg.Issuer),
+		)
 	}
 	return providers
 }
