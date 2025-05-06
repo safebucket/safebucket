a29efb0e63bae67e1da3c08acf6a686aac320657
Renizmy <remi.beltramini@gmail.com>
2025-05-06 19:55:36 +0200

    fix tests

--- internal/tests/handlers_mocks.go
+++ internal/tests/handlers_mocks.go
@@ -2,7 +2,6 @@ package tests
 
 import (
 	"context"
-	"github.com/google/uuid"
 	"github.com/stretchr/testify/mock"
 )
 
@@ -27,49 +26,4 @@ func (m *MockOpenIDCallbackFunc) OpenIDCallback(
 ) (string, string, error) {
 	args := m.Called(ctx, providerName, code, nonce)
 	return args.String(0), args.String(1), args.Error(2)
-}
-
-type MockCreateFunc[In any, Out any] struct {
-	mock.Mock
-}
-
-func (m *MockCreateFunc[In, Out]) Create(ids uuid.UUIDs, input In) (Out, error) {
-	args := m.Called(ids, input)
-	return args.Get(0).(Out), args.Error(1)
-}
-
-type MockGetListFunc[Out any] struct {
-	mock.Mock
-}
-
-func (m *MockGetListFunc[Out]) GetList() []Out {
-	args := m.Called()
-	return args.Get(0).([]Out)
-}
-
-type MockGetOneFunc[Out any] struct {
-	mock.Mock
-}
-
-func (m *MockGetOneFunc[Out]) GetOne(ids uuid.UUIDs) (Out, error) {
-	args := m.Called(ids)
-	return args.Get(0).(Out), args.Error(1)
-}
-
-type MockUpdateFunc[In any, Out any] struct {
-	mock.Mock
-}
-
-func (m *MockUpdateFunc[In, Out]) Update(ids uuid.UUIDs, input In) (Out, error) {
-	args := m.Called(ids, input)
-	return args.Get(0).(Out), args.Error(1)
-}
-
-type MockDeleteFunc struct {
-	mock.Mock
-}
-
-func (m *MockDeleteFunc) Delete(ids uuid.UUIDs) error {
-	args := m.Called(ids)
-	return args.Error(0)
 }
