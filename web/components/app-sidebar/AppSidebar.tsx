4964ca2147af2d51d07c004d85354451b3100412
Yohan Le Bricquer <yohan.lebricquer@gmail.com>
2025-06-21 23:22:39 +0100

    Fix CI

--- web/components/app-sidebar/AppSidebar.tsx
+++ web/components/app-sidebar/AppSidebar.tsx
@@ -341,7 +341,7 @@ export const AppSidebar: FC = () => {
                   className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
                 >
                   <Avatar className="h-8 w-8 rounded-lg">
-                    <AvatarImage src={nav.user.avatar} alt={nav.user.name} />
+                    <AvatarImage src={nav.user.avatar} alt="Image" />
                     <AvatarFallback className="rounded-lg">
                       {session?.loggedUser?.email.charAt(0)}
                     </AvatarFallback>
@@ -366,7 +366,7 @@ export const AppSidebar: FC = () => {
                 <DropdownMenuLabel className="p-0 font-normal">
                   <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
                     <Avatar className="h-8 w-8 rounded-lg">
-                      <AvatarImage src={nav.user.avatar} alt={nav.user.name} />
+                      <AvatarImage src={nav.user.avatar} alt="Image" />
                       <AvatarFallback className="rounded-lg">
                         {session?.loggedUser?.email.charAt(0)}
                       </AvatarFallback>
