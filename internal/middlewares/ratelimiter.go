91db39fc0664d8b056a210477c4426b74a768d0c
Yohan Le Bricquer <yohan.lebricquer@gmail.com>
2025-07-07 22:18:18 +0100

    Cleanup

--- internal/middlewares/ratelimiter.go
+++ internal/middlewares/ratelimiter.go
@@ -10,47 +10,39 @@ import (
 	"strconv"
 )
 
-const AuthenticatedRequestsPerSecond = 200
-const UnauthenticatedRequestsPerSecond = 20
+const authenticatedRequestsPerSecond = 200
+const unauthenticatedRequestsPerSecond = 20
+
+func applyRateLimit(w http.ResponseWriter, r *http.Request, cache core.Cache, userIdentifier string, requestsPerSecond int) {
+	retryAfter, err := cache.GetRateLimit(userIdentifier, requestsPerSecond)
+	if err != nil {
+		zap.L().Error("error", zap.Error(err))
+		helpers.RespondWithError(w, 500, []string{"Unexpected Server Error"})
+		return
+	}
+
+	if retryAfter > 0 {
+		r.Header.Set("Retry-After", strconv.Itoa(retryAfter))
+		helpers.RespondWithError(w, 429, []string{"Rate Limit Exceeded"})
+		return
+	}
+}
 
 func RateLimit(cache core.Cache) func(next http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		fn := func(w http.ResponseWriter, r *http.Request) {
 			if isExcluded(r.URL.Path) {
-				ip, _, err := net.SplitHostPort(r.RemoteAddr)
+				ipAddress, _, err := net.SplitHostPort(r.RemoteAddr)
 				if err != nil {
 					zap.L().Error("error", zap.Error(err))
 					helpers.RespondWithError(w, 500, []string{"Unexpected Server Error"})
-					return
 				}
 
-				retryAfter, err := cache.GetRateLimit(ip, UnauthenticatedRequestsPerSecond)
-				if err != nil {
-					zap.L().Error("error", zap.Error(err))
-					helpers.RespondWithError(w, 500, []string{"Unexpected Server Error"})
-					return
-				}
-
-				if retryAfter > 0 {
-					r.Header.Set("Retry-After", strconv.Itoa(retryAfter))
-					helpers.RespondWithError(w, 429, []string{"Rate Limit Exceeded"})
-					return
-				}
+				applyRateLimit(w, r, cache, ipAddress, unauthenticatedRequestsPerSecond)
 			} else {
-				user := r.Context().Value(models.UserClaimKey{}).(models.UserClaims).UserID.String()
+				userId := r.Context().Value(models.UserClaimKey{}).(models.UserClaims).UserID.String()
 
-				retryAfter, err := cache.GetRateLimit(user, AuthenticatedRequestsPerSecond)
-				if err != nil {
-					zap.L().Error("error", zap.Error(err))
-					helpers.RespondWithError(w, 500, []string{"Unexpected Server Error"})
-					return
-				}
-
-				if retryAfter > 0 {
-					r.Header.Set("Retry-After", strconv.Itoa(retryAfter))
-					helpers.RespondWithError(w, 429, []string{"Rate Limit Exceeded"})
-					return
-				}
+				applyRateLimit(w, r, cache, userId, authenticatedRequestsPerSecond)
 			}
 			next.ServeHTTP(w, r)
 		}
