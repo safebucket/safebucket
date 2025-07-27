58279d1ad097b00d3b8a357abbaae2117681d29e
Yohan Le Bricquer <yohan.lebricquer@gmail.com>
2025-07-27 21:16:11 +0100

    again

--- internal/services/invite.go
+++ internal/services/invite.go
@@ -44,8 +44,6 @@ func (s InviteService) Routes() chi.Router {
 	r.With(m.Validate[models.InviteBody]).Post("/", handlers.CreateHandler(s.CreateInvite))
 
 	r.Route("/{id0}", func(r chi.Router) {
-		r.Get("/", handlers.GetOneHandler(s.GetInvite)) //Todo: don't return the email :)
-
 		r.With(m.Validate[models.InviteChallengeCreateBody]).Post("/challenges", handlers.CreateHandler(s.CreateInviteChallenge))
 
 		r.Route("/challenges/{id1}", func(r chi.Router) {
@@ -54,147 +52,6 @@ func (s InviteService) Routes() chi.Router {
 	})
 
 	return r
-}
-
-func (s InviteService) GetInvite(_ models.UserClaims, ids uuid.UUIDs) (interface{}, error) {
-	// TODO: Implement the logic to fetch an invite by ID.
-	inviteId := ids[0]
-	var invite models.Invite
-	result := s.DB.Where("id = ?", inviteId).First(&invite)
-
-	if result.RowsAffected == 0 {
-		return invite, errors.NewAPIError(401, "INVITE_NOT_FOUND") // Todo: change return code to 404 ?
-	}
-	return nil, nil
-}
-
-func (s InviteService) CreateInviteChallenge(_ models.UserClaims, ids uuid.UUIDs, body models.InviteChallengeCreateBody) (interface{}, error) {
-	inviteId := ids[0]
-	var invite models.Invite
-	result := s.DB.Where("id = ?", inviteId).First(&invite)
-
-	if result.RowsAffected == 0 {
-		return invite, errors.NewAPIError(401, "INVITE_NOT_FOUND") // Todo: change return code to 404 and only log errors ?
-	} else if invite.Email != body.Email {
-		return invite, errors.NewAPIError(401, "INVITE_EMAIL_MISMATCH") //Todo: In the frontend, "an email has been sent if the email is linked to this invitation".
-	} else {
-		// Seed the random number generator
-		var secret string
-
-		for range 6 {
-			secret += strconv.Itoa(rand.Intn(10)) // Add Chars in this var ?
-		}
-		hashedSecret, err := helpers.CreateHash(secret)
-
-		if err != nil {
-			return invite, errors.NewAPIError(500, "INVITE_CHALLENGE_CREATION_FAILED")
-		}
-
-		// Create a new challenge for the invite
-		challenge := models.Challenge{
-			InviteID:     invite.ID,
-			HashedSecret: hashedSecret,
-		}
-
-		result = s.DB.Create(&challenge)
-		if result.Error != nil {
-			return invite, errors.NewAPIError(500, "INVITE_CHALLENGE_CREATION_FAILED")
-		}
-
-		event := events.NewChallengeUserInvite(
-			*s.Publisher,
-			secret,
-			invite.Email,
-			inviteId.String(),
-			challenge.ID.String(),
-			s.WebUrl,
-		)
-		event.Trigger()
-
-		return challenge, nil // TODO: In the frontend, "an email has been sent if the email is linked to this invitation".
-
-	}
-}
-
-func (s InviteService) ValidateInviteChallenge(_ models.UserClaims, ids uuid.UUIDs, body models.InviteChallengeValidateBody) (models.AuthLoginResponse, error) {
-	inviteId := ids[0]
-	challengeId := ids[1]
-
-	var challenge models.Challenge
-
-	result := s.DB.Preload("Invite").Where("id = ? AND invite_id = ?", challengeId, inviteId).First(&challenge)
-
-	if result.RowsAffected == 0 {
-		return models.AuthLoginResponse{}, errors.NewAPIError(404, "CHALLENGE_NOT_FOUND")
-	}
-
-	match, err := argon2id.ComparePasswordAndHash(body.Code, challenge.HashedSecret)
-	if err != nil || !match {
-		return models.AuthLoginResponse{}, errors.NewAPIError(401, "WRONG_CODE")
-	}
-
-	// If the code matches, we create a new user, create policies for the user, and return the access token.
-	newUser := models.User{
-		Email: challenge.Invite.Email,
-	}
-
-	zap.L().Info("Creating new user", zap.Any("user", newUser))
-
-	result = s.DB.Where("email = ?", newUser.Email).First(&newUser)
-	if result.RowsAffected == 0 {
-		// Create a new user
-		s.DB.Create(&newUser) // No password is set, as this is an invite
-		zap.L().Info("User inserted", zap.Any("user", newUser))
-		err = roles.AddUserToRoleGuest(s.Enforcer, newUser)
-		if err != nil {
-			zap.L().Error("can not add user to role user", zap.Error(err))
-		}
-		err = roles.AllowUserToSelfModify(s.Enforcer, newUser) // Allow the user to modify their own data (reset password, etc.)
-		if err != nil {
-			zap.L().Error("can not allow user to self modify", zap.Error(err))
-		}
-
-		var invites []models.Invite
-
-		s.DB.Preload("Bucket").Where("email = ?", challenge.Invite.Email).Find(&invites)
-
-		for _, invite := range invites {
-			zap.L().Info("User inserted", zap.Any("user", newUser))
-			var err error
-			switch invite.Group {
-			case "viewer":
-				err = groups.AddUserToViewers(s.Enforcer, invite.Bucket, newUser.ID.String())
-			case "contributor":
-				err = groups.AddUserToContributors(s.Enforcer, invite.Bucket, newUser.ID.String())
-			case "owner":
-				err = groups.AddUserToOwners(s.Enforcer, invite.Bucket, newUser.ID.String())
-			default:
-				zap.L().Error("Invalid group in invite", zap.String("group", invite.Group), zap.String("bucket_id", invite.BucketID.String()), zap.String("user_id", invite.CreatedBy.String()))
-			}
-			if err != nil {
-				zap.L().Error("Failed to add user to group", zap.Error(err), zap.String("group", invite.Group), zap.String("bucket_id", invite.BucketID.String()), zap.String("user_id", invite.CreatedBy.String()))
-			}
-			s.DB.Delete(&invite)
-		}
-
-		accessToken, err := h.NewAccessToken(s.JWTConf.Secret, &newUser, configuration.AuthLocalProviderName)
-		if err != nil {
-			return models.AuthLoginResponse{}, errors.NewAPIError(500, "GENERATE_ACCESS_TOKEN_FAILED")
-		}
-		refreshToken, err := h.NewRefreshToken(s.JWTConf.Secret, &newUser, configuration.AuthLocalProviderName)
-		if err != nil {
-			return models.AuthLoginResponse{}, errors.NewAPIError(500, "GENERATE_REFRESH_TOKEN_FAILED")
-		}
-
-		tokens := models.AuthLoginResponse{
-			AccessToken:  accessToken,
-			RefreshToken: refreshToken,
-		}
-		return tokens, err
-
-	} else {
-		return models.AuthLoginResponse{}, errors.NewAPIError(401, "USER_ALREADY_EXISTS")
-	}
 }
 
 func (s InviteService) CreateInvite(user models.UserClaims, ids uuid.UUIDs, body models.InviteBody) ([]models.InviteResult, error) {
@@ -364,4 +221,133 @@ func (s InviteService) CreateInvite(user models.UserClaims, ids uuid.UUIDs, body
 	}
 
 	return results, nil // TODO: Normalize messages ?
+}
+
+func (s InviteService) CreateInviteChallenge(_ models.UserClaims, ids uuid.UUIDs, body models.InviteChallengeCreateBody) (interface{}, error) {
+	inviteId := ids[0]
+	var invite models.Invite
+	result := s.DB.Where("id = ?", inviteId).First(&invite)
+
+	if result.RowsAffected == 0 {
+		return invite, errors.NewAPIError(401, "INVITE_NOT_FOUND") // Todo: change return code to 404 and only log errors ?
+	} else if invite.Email != body.Email {
+		return invite, errors.NewAPIError(401, "INVITE_EMAIL_MISMATCH") //Todo: In the frontend, "an email has been sent if the email is linked to this invitation".
+	} else {
+		// Seed the random number generator
+		var secret string
+
+		for range 6 {
+			secret += strconv.Itoa(rand.Intn(10)) // Add Chars in this var ?
+		}
+		hashedSecret, err := helpers.CreateHash(secret)
+
+		if err != nil {
+			return invite, errors.NewAPIError(500, "INVITE_CHALLENGE_CREATION_FAILED")
+		}
+
+		// Create a new challenge for the invite
+		challenge := models.Challenge{
+			InviteID:     invite.ID,
+			HashedSecret: hashedSecret,
+		}
+
+		result = s.DB.Create(&challenge)
+		if result.Error != nil {
+			return invite, errors.NewAPIError(500, "INVITE_CHALLENGE_CREATION_FAILED")
+		}
+
+		event := events.NewChallengeUserInvite(
+			*s.Publisher,
+			secret,
+			invite.Email,
+			inviteId.String(),
+			challenge.ID.String(),
+			s.WebUrl,
+		)
+		event.Trigger()
+
+		return challenge, nil // TODO: In the frontend, "an email has been sent if the email is linked to this invitation".
+
+	}
+}
+
+func (s InviteService) ValidateInviteChallenge(_ models.UserClaims, ids uuid.UUIDs, body models.InviteChallengeValidateBody) (models.AuthLoginResponse, error) {
+	inviteId := ids[0]
+	challengeId := ids[1]
+
+	var challenge models.Challenge
+
+	result := s.DB.Preload("Invite").Where("id = ? AND invite_id = ?", challengeId, inviteId).First(&challenge)
+
+	if result.RowsAffected == 0 {
+		return models.AuthLoginResponse{}, errors.NewAPIError(404, "CHALLENGE_NOT_FOUND")
+	}
+
+	match, err := argon2id.ComparePasswordAndHash(body.Code, challenge.HashedSecret)
+	if err != nil || !match {
+		return models.AuthLoginResponse{}, errors.NewAPIError(401, "WRONG_CODE")
+	}
+
+	// If the code matches, we create a new user, create policies for the user, and return the access token.
+	newUser := models.User{
+		Email: challenge.Invite.Email,
+	}
+
+	zap.L().Info("Creating new user", zap.Any("user", newUser))
+
+	result = s.DB.Where("email = ?", newUser.Email).First(&newUser)
+	if result.RowsAffected == 0 {
+		// Create a new user
+		s.DB.Create(&newUser) // No password is set, as this is an invite
+		zap.L().Info("User inserted", zap.Any("user", newUser))
+		err = roles.AddUserToRoleGuest(s.Enforcer, newUser)
+		if err != nil {
+			zap.L().Error("can not add user to role user", zap.Error(err))
+		}
+		err = roles.AllowUserToSelfModify(s.Enforcer, newUser) // Allow the user to modify their own data (reset password, etc.)
+		if err != nil {
+			zap.L().Error("can not allow user to self modify", zap.Error(err))
+		}
+
+		var invites []models.Invite
+
+		s.DB.Preload("Bucket").Where("email = ?", challenge.Invite.Email).Find(&invites)
+
+		for _, invite := range invites {
+			zap.L().Info("User inserted", zap.Any("user", newUser))
+			var err error
+			switch invite.Group {
+			case "viewer":
+				err = groups.AddUserToViewers(s.Enforcer, invite.Bucket, newUser.ID.String())
+			case "contributor":
+				err = groups.AddUserToContributors(s.Enforcer, invite.Bucket, newUser.ID.String())
+			case "owner":
+				err = groups.AddUserToOwners(s.Enforcer, invite.Bucket, newUser.ID.String())
+			default:
+				zap.L().Error("Invalid group in invite", zap.String("group", invite.Group), zap.String("bucket_id", invite.BucketID.String()), zap.String("user_id", invite.CreatedBy.String()))
+			}
+			if err != nil {
+				zap.L().Error("Failed to add user to group", zap.Error(err), zap.String("group", invite.Group), zap.String("bucket_id", invite.BucketID.String()), zap.String("user_id", invite.CreatedBy.String()))
+			}
+			s.DB.Delete(&invite)
+		}
+
+		accessToken, err := h.NewAccessToken(s.JWTConf.Secret, &newUser, configuration.AuthLocalProviderName)
+		if err != nil {
+			return models.AuthLoginResponse{}, errors.NewAPIError(500, "GENERATE_ACCESS_TOKEN_FAILED")
+		}
+		refreshToken, err := h.NewRefreshToken(s.JWTConf.Secret, &newUser, configuration.AuthLocalProviderName)
+		if err != nil {
+			return models.AuthLoginResponse{}, errors.NewAPIError(500, "GENERATE_REFRESH_TOKEN_FAILED")
+		}
+
+		tokens := models.AuthLoginResponse{
+			AccessToken:  accessToken,
+			RefreshToken: refreshToken,
+		}
+		return tokens, err
+
+	} else {
+		return models.AuthLoginResponse{}, errors.NewAPIError(401, "USER_ALREADY_EXISTS")
+	}
 }
