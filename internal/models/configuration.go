61a9f89c89962fc6955ebf5cb8c31f742219a3ca
Yohan Le Bricquer <yohan.lebricquer@gmail.com>
2025-08-30 17:15:13 +0100

    Typo

--- internal/models/configuration.go
+++ internal/models/configuration.go
@@ -134,7 +134,7 @@ type ActivityConfiguration struct {
 }
 
 type LokiConfiguration struct {
-	Type     string `mapstructure:"type" validate:"required,oneof=smtp"`
+	Type     string `mapstructure:"type" validate:"required,oneof=loki"`
 	Endpoint string `mapstructure:"endpoint" validate:"required"`
 }
 
