964023efc870b52b0c46ea76526aad5bced60328
Yohan Le Bricquer <yohan.lebricquer@gmail.com>
2025-09-22 08:47:55 +0100

    Change file_status to enum

--- internal/database/sql.go
+++ internal/database/sql.go
@@ -24,7 +24,20 @@ func InitDB(config models.DatabaseConfiguration) *gorm.DB {
 }
 
 func runMigrations(db *gorm.DB) {
-	err := db.AutoMigrate(&models.User{}, &models.Bucket{}, &models.File{}, &models.Invite{}, &models.Challenge{})
+	var exists bool
+	err := db.Raw("select exists(select 1 from pg_type where typname = 'file_status')").Scan(&exists).Error
+	if err != nil {
+		zap.L().Error("failed to check if file_status enum exists", zap.Error(err))
+	}
+
+	if !exists {
+		err = db.Exec("CREATE TYPE file_status AS ENUM ('uploading', 'uploaded', 'deleting')").Error
+		if err != nil {
+			zap.L().Error("failed to create file_status enum", zap.Error(err))
+		}
+	}
+
+	err = db.AutoMigrate(&models.User{}, &models.Bucket{}, &models.File{}, &models.Invite{}, &models.Challenge{})
 	if err != nil {
 		zap.L().Error("failed to migrate db models", zap.Error(err))
 	}
