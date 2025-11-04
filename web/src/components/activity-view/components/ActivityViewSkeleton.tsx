132eb14274fcd09e209547966d7551b45278cf34
Yohan Le Bricquer <yohan.lebricquer@gmail.com>
2025-11-04 19:52:58 +0000

    Improve skeleton

--- web/src/components/activity-view/components/ActivityViewSkeleton.tsx
+++ web/src/components/activity-view/components/ActivityViewSkeleton.tsx
@@ -16,6 +16,7 @@ export function ActivityViewSkeleton({ count = 5 }: ActivityViewSkeletonProps) {
           <div className="flex-1 space-y-2">
             <Skeleton className="h-4 w-3/4" />
             <Skeleton className="h-3 w-1/2" />
+            <Skeleton className="h-3 w-24" />
           </div>
         </div>
       ))}
