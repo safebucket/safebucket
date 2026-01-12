3f7fe66e76ad902a531de39b01deabe78727461c
yohanleb <28931821+yohanleb@users.noreply.github.com>
2026-01-12 18:02:43 +0000

    test

--- web/src/components/activity-view/ActivityView.tsx
+++ web/src/components/activity-view/ActivityView.tsx
@@ -23,9 +23,7 @@ export function ActivityView({ activity }: ActivityViewProps) {
     <ItemGroup>
       {activity.map((item, index) => (
         <div key={index}>
-          <ActivityItem item={item} />
-          {index < activity.length - 1 && <ItemSeparator />}
-        </div>
+          <ActivityItem item={item} />{index < activity.length - 1 && <ItemSeparator />}</div>
       ))}
     </ItemGroup>
   );
