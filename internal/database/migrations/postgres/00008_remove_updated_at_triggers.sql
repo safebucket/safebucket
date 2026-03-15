738938c02d1f9dabab9dfc1833c8e0583e4bef11
Renizmy <remi.beltramini@gmail.com>
2026-03-15 20:38:26 +0100

    fix

--- internal/database/migrations/postgres/00008_remove_updated_at_triggers.sql
+++ internal/database/migrations/postgres/00008_remove_updated_at_triggers.sql
@@ -5,46 +5,5 @@ DROP TRIGGER IF EXISTS update_memberships_updated_at ON memberships;
 DROP TRIGGER IF EXISTS update_folders_updated_at ON folders;
 DROP TRIGGER IF EXISTS update_files_updated_at ON files;
 DROP FUNCTION IF EXISTS update_updated_at_column();
-
 -- +goose Down
--- +goose StatementBegin
-
-CREATE OR REPLACE FUNCTION update_updated_at_column() RETURNS TRIGGER AS
-$$
-BEGIN
-    new.updated_at = CURRENT_TIMESTAMP;
-    RETURN new;
-END;
-$$ LANGUAGE 'plpgsql';
-
-CREATE TRIGGER update_users_updated_at
-    BEFORE UPDATE
-    ON users
-    FOR EACH ROW
-EXECUTE FUNCTION update_updated_at_column();
-
-CREATE TRIGGER update_buckets_updated_at
-    BEFORE UPDATE
-    ON buckets
-    FOR EACH ROW
-EXECUTE FUNCTION update_updated_at_column();
-
-CREATE TRIGGER update_memberships_updated_at
-    BEFORE UPDATE
-    ON memberships
-    FOR EACH ROW
-EXECUTE FUNCTION update_updated_at_column();
-
-CREATE TRIGGER update_folders_updated_at
-    BEFORE UPDATE
-    ON folders
-    FOR EACH ROW
-EXECUTE FUNCTION update_updated_at_column();
-
-CREATE TRIGGER update_files_updated_at
-    BEFORE UPDATE
-    ON files
-    FOR EACH ROW
-EXECUTE FUNCTION update_updated_at_column();
-
--- +goose StatementEnd
+-- no-op: triggers are not recreated
