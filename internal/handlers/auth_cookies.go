207c2a52b37efa56f1bb73af5712b4bc61e835c9
yohanleb <28931821+yohanleb@users.noreply.github.com>
2026-05-13 11:07:13 +0100

    typo

--- internal/handlers/auth_cookies.go
+++ internal/handlers/auth_cookies.go
@@ -27,7 +27,7 @@ func isSecureRequest(r *http.Request, forceSecure bool) bool {
 }
 
 func longLivedCookie(name, value string, secure bool) *http.Cookie {
-	return &http.Cookie{ //nolint:gosec // G124: clearing cookie, Secure not needed for expiry.
+	return &http.Cookie{ //nolint:gosec // G124: Secure is set conditionally based on TLS/forceSecure.
 		Name:     name,
 		Value:    value,
 		Expires:  time.Now().Add(authCookieDuration),
@@ -63,7 +63,7 @@ func BuildAccessCookie(isSecure bool, access string) []*http.Cookie {
 }
 
 func BuildMFACookie(isSecure bool, token string) []*http.Cookie {
-	mfa := &http.Cookie{ //nolint:gosec // G124: clearing cookie, Secure not needed for expiry.
+	mfa := &http.Cookie{ //nolint:gosec // G124: Secure is set conditionally based on TLS/forceSecure.
 		Name:     configuration.CookieMFAToken,
 		Value:    token,
 		MaxAge:   mfaCookieMaxAgeSeconds,
@@ -81,7 +81,7 @@ func BuildMFACookie(isSecure bool, token string) []*http.Cookie {
 }
 
 func BuildShareCookie(isSecure bool, shareID, token string) []*http.Cookie {
-	return []*http.Cookie{{ //nolint:gosec // G124: clearing cookie, Secure not needed for expiry.
+	return []*http.Cookie{{ //nolint:gosec // G124: Secure is set conditionally based on TLS/forceSecure.
 		Name:     configuration.CookieShareToken,
 		Value:    token,
 		MaxAge:   shareCookieMaxAgeSeconds,
