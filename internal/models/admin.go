bfc5029d6672ae68e7794ba9bce3de6020ce6eec
Renizmy <remi.beltramini@gmail.com>
2026-08-27 01:49:26 +0200

    fix linter

--- internal/models/admin.go
+++ internal/models/admin.go
@@ -21,11 +21,11 @@ type AdminActivityQueryParams struct {
 }
 
 type AdminStatsResponse struct {
-	TotalUsers         int64             `json:"total_users"`
-	TotalBuckets       int64             `json:"total_buckets"`
-	TotalFiles         int64             `json:"total_files"`
-	TotalFolders       int64             `json:"total_folders"`
-	TotalStorageBytes  int64             `json:"total_storage"`
+	TotalUsers           int64             `json:"total_users"`
+	TotalBuckets         int64             `json:"total_buckets"`
+	TotalFiles           int64             `json:"total_files"`
+	TotalFolders         int64             `json:"total_folders"`
+	TotalStorageBytes    int64             `json:"total_storage"`
 	ActiveStorageBytes   int64             `json:"active_storage"`
 	InactiveStorageBytes int64             `json:"inactive_storage"`
 	SharedFilesPerHour   []TimeSeriesPoint `json:"shared_files_per_hour"`
