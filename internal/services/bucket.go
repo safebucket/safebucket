1bbb4fe268bc16052e169915b60d12c457364e8e
yohanleb <28931821+yohanleb@users.noreply.github.com>
2026-08-28 19:34:56 +0100

    chore: refactor trash endpoint to its own resource

--- internal/services/bucket.go
+++ internal/services/bucket.go
@@ -201,6 +201,9 @@ func (s BucketService) GetBucket(
 	if err != nil {
 		return bucket, err
 	}
+	bucket.Files = []models.File{}
+	bucket.Folders = []models.Folder{}
+
 	now := time.Now()
 	expirationTime := now.Add(-c.UploadPolicyExpirationInMinutes * time.Minute)
 
